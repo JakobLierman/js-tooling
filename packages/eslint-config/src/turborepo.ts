123495ddb8f8b256a7fc1fd31f5029a9fce20587
Jakob Lierman <jakob.lierman@gmail.com>
2026-02-23 11:48:43 +0100

    chore: migrate tsup to tsdown

--- packages/eslint-config/src/turborepo.ts
+++ packages/eslint-config/src/turborepo.ts
@@ -3,20 +3,23 @@ import { type ConfigObject } from '@eslint/core';
 import { configs as turboPluginConfigs } from 'eslint-plugin-turbo';
 import { importNoExtraneousDependenciesConfig } from './import-no-extraneous-dependencies';
 
-const config: ConfigObject[] = defineConfig(
-  turboPluginConfigs['flat/recommended'],
-  {
-    rules: {
-      'turbo/no-undeclared-env-vars': ['error', { allowList: ['CI', 'TZ'] }],
-      'import/no-extraneous-dependencies': [
-        'error',
-        {
-          ...importNoExtraneousDependenciesConfig,
-          packageDir: ['.', '../..'], // Package directories for monorepos
-        },
-      ],
-    },
+const turboRecommendedConfig = turboPluginConfigs?.['flat/recommended'] as
+  | ConfigObject
+  | undefined;
+if (!turboRecommendedConfig)
+  throw new Error('Turbo recommended config not found');
+
+const config: ConfigObject[] = defineConfig(turboRecommendedConfig, {
+  rules: {
+    'turbo/no-undeclared-env-vars': ['error', { allowList: ['CI', 'TZ'] }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        ...importNoExtraneousDependenciesConfig,
+        packageDir: ['.', '../..'], // Package directories for monorepos
+      },
+    ],
   },
-);
+});
 
 export default config;
